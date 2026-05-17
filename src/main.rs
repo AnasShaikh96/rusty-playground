3c41104d6480895b023052390a3105a7c7cf97de
Anas Shaikh <shaikhanas246@gmail.com>
2026-05-17 12:15:05 +0530

    destructure and shadow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+
 fn main() {
-    println!("Hello, world!");
+    let (x,y);
+    // destructing
+    (x,..) = (3,4);
+    [..,y] = [1,2];
+
+    assert_eq!([x,y],[3,2]);
+    println!("Success {} {}",x,y);
 }
+
+
