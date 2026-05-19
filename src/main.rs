8750c378aea6c167a8155776e0b8b67497139d9b
Anas Shaikh <shaikhanas246@gmail.com>
2026-05-19 21:38:13 +0530

    range and number types

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
 #[allow(unused_variables)]
 fn main() {
 
-    let mut sum:i32 = 0;
-
-    for i in -3..2 {
-        sum += i;
-    }
-
-    assert!(sum == -5);
-
-    for c in 'A'..='z' {
-        println!("{}", c);
-    }
+    println!("{}", 9_i16 + 251_i16)
 }
 
