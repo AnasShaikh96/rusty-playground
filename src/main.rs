cdf66a059faf9a9dd80cd895d644338209b2d92b
Anas Shaikh <shaikhanas246@gmail.com>
2026-05-19 22:02:59 +0530

    bitwise ops, int and floats

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 #[allow(unused_variables)]
 fn main() {
+   
+    assert!(1_u32 + 2 == 3);
 
-    println!("{}", 9_i16 + 251_i16)
+    assert!(1i32 - 2 == -1);
+    assert!(1i8 - 2 == -1);
+
+
+    assert!(3*50 == 150);
+
+    assert!(9.6 as f32 / 3.2 as f32 == 3.0 as f32);
+
+    assert!(24 % 5 == 4);
+
+
+    println!("Success")
 }
 
