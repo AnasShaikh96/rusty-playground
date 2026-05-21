ad6d7977d604889eee38993e38995135db9ef102
Anas Shaikh <shaikhanas246@gmail.com>
2026-05-21 21:28:32 +0530

    types of functions and match case

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
-
 // use std::mem::size_of_val;
 
 #[allow(unused_variables)]
 fn main() {
-    let _v : () = ();
+    never_return();
+    println!("Success",)
+}
 
-    let v:(i32,i32) = (3,4);
-    assert_eq!(_v, implicitly_ret_unit());
-    println!("Success")
+fn never_return() -> ! {
+    todo!();
 }
 
 
-fn implicitly_ret_unit(){
-    println!("I will return ()");
-}
+
