8be06acfa8514ae443a96f9730f3936985a56913
Anas Shaikh <shaikhanas246@gmail.com>
2026-05-26 22:24:37 +0530

    borrowing exercise

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,16 @@
 
 #[allow(unused_variables)]
 fn main() {
+    let s1 = String::from("Hello, World");
+    let s2 = take_ownership(s1);
 
-    let s = String::from("Hello");
-    take_ownership(&s);
-    println!("Success s! {}", s);
-
-
-    let x = 5;
-    makes_copy(x);
-    println!("Success! {}", x)
+    println!("{}", s2);    
 }
 
-fn take_ownership(s: &String) -> String {
-    println!("Printed param {}", s);
-    s.to_string()
+fn take_ownership(s:String) ->String {
+    println!("{}",s);
+    s
 }
-
-
-fn makes_copy(x:i32){
-    println!("Copy wala fn {}", x)
-}
-
-
-
-
-
 
 
 
