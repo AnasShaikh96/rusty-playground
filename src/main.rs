e85b44a7909172106ad56e3a97b714d4499f32f9
Anas Shaikh <shaikhanas246@gmail.com>
2026-06-01 20:27:12 +0530

    String and &str types

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,12 @@
 
 #[allow(unused_variables)]
 fn main() {
-    let c = '%';
+    for c in "#$%^&".chars() {
+        println!("{}",c)
+    }
 
-    let r1 = &c;
-    let ref r2 = c;
-
-    assert_eq!(*r1,*r2);
-    assert_eq!(get_address(&r1),get_address(&r2));
-
-    println!("Success")
 }
 
-fn get_address(c: &char) -> String{
-    format!("{:p}",c)
-}
 
 
 
