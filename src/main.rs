7c50cc21179f0cc5b5d2a43151327a78a060bf7a
Anas Shaikh <shaikhanas246@gmail.com>
2026-06-02 21:46:14 +0530

    Array methods and size

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@
 
 #[allow(unused_variables)]
 fn main() {
-    for c in "#$%^&".chars() {
-        println!("{}",c)
-    }
+    let list : [i32;100] =[1;100];
 
+    assert!(list[0] == 1);
+    assert!(list.len() == 100);
+    
+    println!("Success")
 }
 
 
