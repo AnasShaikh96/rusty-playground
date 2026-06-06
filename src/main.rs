ebaee6a920291859996a0478fa93d45c565e8f3c
Anas Shaikh <shaikhanas246@gmail.com>
2026-06-06 13:13:48 +0530

    calc in rust

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,43 @@
 
 #[allow(unused_variables)]
 fn main() {
-    let a  = [1,2,3];
-    let s = "Hello, world";
-    let s1 = &a[0..2];
-    let s2 = &s[2..6];
-    
-    println!("Success {:?}",s2)
-
+    let calculate = basic_calc(&[2,3],&"div");
+    println!("Answer: {}",calculate)  
 }
 
 
+fn basic_calc(nums: &[i32;2], op:&str) -> i32 {
+    let mut output = 0;
+    match op {
+        "add" => {
+            for n in nums {
+                output += n;
+
+            }
+        },
+        "sub" => {
+            for n in nums {
+                output -= n;
+
+            }
+        },
+        "mul" => {
+            output += 1;
+            for n in nums {
+                output *= n;
+            }
+        },
+        "div" => {
+            output += 1;
+            for n in nums {
+              return  output % n
+            }
+        }
+        &_ => println!("nothing happened")
+    }
+   println!("{}",output);
+    output
+}
 
 
 
