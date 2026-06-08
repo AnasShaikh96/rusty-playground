c3a36901851d1ee33706a9d6909ad8fdf336c77b
Anas Shaikh <shaikhanas246@gmail.com>
2026-06-08 21:29:12 +0530

    find largest num

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,10 @@
 
 #[allow(unused_variables)]
 fn main() {
-    let largest = find_largest_num(&[6,1,330,4,9]);
-    println!("Answer: {}",largest)  
+    let t: (u8,u16,i64,str,String) = (1u8,2u16,3i64,"hello",String::from("World"));
+    println!("Success")
 }
 
-
-fn find_largest_num(arr: &[u32;5]) -> u32{
-    let mut num = 0;
-    for n in arr {
-        if num < *n {
-            num = *n;
-        }
-    }
-    num
-}
 
 
 
