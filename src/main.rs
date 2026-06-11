8c8091598765630a688a2275e023c6dd5d611812
Anas Shaikh <shaikhanas246@gmail.com>
2026-06-11 21:12:15 +0530

    struct spread ops

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
 // use std::mem::size_of_val;
 
 #[allow(unused_variables)]
-fn main() {
-    struct User {
+struct User {
         name: String,
         number: u64,
         age: u16,
         email: String
     }
-
-    let user = User {
+fn main() {
+    let user1 = User {
         name: String::from("John Doe"),
         number: 123456789,
         age: 60,
         email: String::from("some@admin.com")
     };
 
-    println!("Success {}", user.name)
+    let user2 = User {
+        email:String::from("someuser@admin.com"),
+        ..user1
+    };
+
+    println!("Success {}", user2.email);
 }
 
 
